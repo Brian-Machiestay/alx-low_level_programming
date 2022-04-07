f9f07ae4f43ca9241994596c5ac1316b56ffb02d
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-07 21:01:32 +0000

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	char this_string[] = "with proper grammar, but the outcome is a piece of art,";
-	
-	printf(%s, this_string);
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
