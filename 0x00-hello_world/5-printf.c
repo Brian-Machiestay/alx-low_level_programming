3667e42f49c691262501709c5a4c952a4b083fd0
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-07 20:55:03 +0000

    sixth

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * this function prints a string
+ */
+int main(void)
+{
+	char this_string[] = "with proper grammar, but the outcome is a piece of art,";
+	
+	printf(%s, this_string);
+}
