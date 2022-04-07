ed0b4ecb723b8478ed96679d144a9df5b9889a8c
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-07 19:57:24 +0000

    first C program, so much fun

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <string.h>
+int main(void)
+{
+	char some_string[]="\"Programming is like building a multilingual puzzle";
+        puts(some_string);
+        return 0;
+}
