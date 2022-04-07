cf6a5c39c3767bf8a67859a98851166baa36bbd3
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-07 20:39:59 +0000

    its nastier than I thought

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include <string.h>
 /* This code prints to standard output using put */
+
+/**
+ * function_name - main
+ *
+ * @parameterx: void
+ * Description: nothing really
+ * return: integer
+ */
 int main(void)
 {
 	char some_string[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(some_string);
-	return(0);
+	return (0);
 }
