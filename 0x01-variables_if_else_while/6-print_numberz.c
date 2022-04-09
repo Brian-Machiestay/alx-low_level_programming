7ea6a81bc435ae1cde115e123564cebd4ed6a540
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-09 14:52:11 +0000

    corrected four

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-#include <assert.h>
 /**
- *main-function begins
+ *main- function begins
  *
  *Return: function ends
  */
 int main(void)
 {
-	char digits[11] = "0123456789";
-	int i = 0;
-	while (i < 10)
-	{
-		putchar("%c", digits[i]);
-		i++;
-	}
-	putchar('\n');
-	return (0);
+	
 }
