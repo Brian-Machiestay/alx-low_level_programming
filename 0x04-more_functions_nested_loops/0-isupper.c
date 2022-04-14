98e8634c56a472f91ef8456cc7e54f293257c4d9
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-14 20:05:44 +0000

    first to fifth file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * _isupper - check the code.
+ *_isupper - check the code.
  *@c: only integer parameter
  * Return: Always 0.
  */
