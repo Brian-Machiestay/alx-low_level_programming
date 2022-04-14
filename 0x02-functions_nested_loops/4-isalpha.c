41cf3079a4ea08ac19feef508dbc8e4bf44ebd1a
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-14 01:55:08 +0000

    hmm

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,29 +2,6 @@
 #include <ctype.h>
 #include "main.h"
 /**
- * main - check the code.
- *@c: first member
- * Return: Always 0.
- */
-
-int _isalpha(int c);
-
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	putchar(r + '0');
-	r = _isalpha('o');
-	putchar(r + '0');
-	r = _isalpha(108);
-	putchar(r + '0');
-	r = _isalpha(';');
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
-}
-/**
  * _isalpha - check the code.
  *@c: integer return type
  * Return: Always 0.
