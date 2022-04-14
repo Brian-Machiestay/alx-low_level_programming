b8bfd8cff1501918749da245c9a05df7fab64580
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-14 04:00:29 +0000

    hmm

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "main.h"
+/**
+ *void print_to_98 - check the code.
+ *@n: integer return type
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (int i = n; i < 99; i++)
+	{
+		putchar (i + '0');
+		if (i != 98)
+		{
+			putchar (',');
+			putchar (' ');
+			putchar (' ');
+		}
+	}
+}
