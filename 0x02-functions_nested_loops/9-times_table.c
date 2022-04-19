5b978e907baeabf2fa63f9fe802a7a152b88ba8f
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-19 12:57:56 +0000

    corrected the times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
+			        _putchar(' ');
 			}
 			else
 			{
@@ -39,6 +39,6 @@ void times_table(void)
 				_putchar(' ');
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
