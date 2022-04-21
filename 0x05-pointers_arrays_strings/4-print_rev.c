986228e12a1ade9035fd020aa24167d571c6990f
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-21 02:19:26 +0000

    task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *print_rev - check the code
+ *
+ *@s: a pointer variable
+ *
+ */
+void print_rev(char *s)
+{
+	char c = *s;
+	int i;
+	int size = strlen(s);
+
+	for (i = size - 1; i > -1; i--)
+	{
+		putchar(*(s + i));
+	}
+	putchar('\n');
+}
