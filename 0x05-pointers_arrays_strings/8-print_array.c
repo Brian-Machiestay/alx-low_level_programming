d7dfc1a6b63e1a61658ca5c9cdc439d086f17f35
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-21 05:04:24 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ *print_array - check the code
+ *
+ *@a: a pointer variable
+ *@n:  integer variable
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *(a + i));
+		if (n < n - 1)
+		{
+			printf(", ");
+		}
+	}
+}
