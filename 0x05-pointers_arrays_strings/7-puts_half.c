839589f6f39176e64ab79fefb66a7509ed4ac38a
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-21 04:41:26 +0000

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,11 +14,13 @@ void puts_half(char *str)
 	int n;
 
 	n = size / 2;
+	i = n;
 	if (size % 2 != 0)
 	{
 		n = (size - 1) / 2;
+		i = n - 1;
 	}
-	for (i = n; i < size; i++)
+	for (; i < size; i++)
 	{
 		putchar(*(str + i));
 	}
