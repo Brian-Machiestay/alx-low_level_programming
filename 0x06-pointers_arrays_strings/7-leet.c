3ab9b7625b265eec0cf2e1b715f8e907e95ab087
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-23 14:25:40 +0000

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,19 +3,22 @@
 
 /**
  *leet - change all lower case to upper
+ *
  *@a: a pointer variable
+ *
  *Return: a pointer
  */
 char *leet(char *a)
 {
 	int i;
 	int j;
+	char m;
 	char *toencode = "aBeEoOtTlL";
 	char *encode = "4433007711";
 
 	for (i = 0; *(a + i) != '\0'; i++)
 	{
-		char m = *(a + i);
+		m = *(a + i);
 
 		for (j = 0; j < 10; j++)
 		{
