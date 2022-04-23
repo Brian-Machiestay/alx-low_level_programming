cdd8b41769557921f7527e1a52aea1280e69bbc2
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-04-23 14:51:21 +0000

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,19 +12,17 @@ char *leet(char *a)
 {
 	int i;
 	int j;
-	char m;
-	char *toencode = "aBeEoOtTlL";
-	char *encode = "4433007711";
+	char toencode[] = "aBeEoOtTlL";
+	char encode[] = "4433007711";
 
-	for (i = 0; *(a + i) != '\0'; i++)
+	for (i = 0; a[i]; i++)
 	{
-		m = *(a + i);
 
 		for (j = 0; j < 10; j++)
 		{
-			if (m == *(toencode + j))
+			if (a[i] == toencode[j])
 			{
-				*(a + i) = *(encode + j);
+				a[i] = encode[j];
 				break;
 			}
 		}
