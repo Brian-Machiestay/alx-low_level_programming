48e7c23534e20794114e203f81ffaf0ed63d5ce1
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-05 03:35:50 +0000

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: an int
+ * @argv: an array
+ * Return: an integer
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
