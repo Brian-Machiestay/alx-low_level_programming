208eca57b1a54e1bcd4e2fc472a21e1e5fe54015
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-05 03:25:17 +0000

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: an int
+ * @argv: an array
+ * Return: an integer
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - argc]);
+	return (0);
+}
