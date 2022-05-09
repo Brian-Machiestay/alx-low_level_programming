bcb4e76ecb12b80e55a51567089b29f1fd2170f3
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-09 19:56:42 +0000

    task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - returns memory to an array
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arraymem;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arraymem = malloc(nmemb * size);
+	if (arraymem == NULL)
+		return (NULL);
+	return (arraymem);
+}
