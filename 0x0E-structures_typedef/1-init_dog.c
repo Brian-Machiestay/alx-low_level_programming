6284862ff60adb2010eff9f0cc86c2d1ca4b08a6
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-12 18:15:26 +0000

    task 0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**1
+ * init_dog - check the code
+ * @name: dog's name
+ * @d: the dog object
+ * @age: the dog's age
+ * @owner: the dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
