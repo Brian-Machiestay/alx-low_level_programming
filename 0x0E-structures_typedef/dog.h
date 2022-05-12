5c0061bb856db79c0b72c739bad1039772bd70ea
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-12 08:03:25 +0000

    begining of things

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - check the code
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: the owner of this dog
+ */
 struct dog
 {
-	char * name;
+	char *name;
 	float age;
 	char *owner;
 };
