7cbfdfe66dacef2e79c75d8af5c9f1acb55ae735
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-12 04:55:03 +0000

    task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,5 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
+#ifndef STDLIB_H
 #include <stdlib.h>
-#define ABS(x) abs((x))
+#endif /* STDLIB_H */
+#define ABS(x) abs(x)
 #endif /* FUNCTION_LIKE_MACRO_H */
