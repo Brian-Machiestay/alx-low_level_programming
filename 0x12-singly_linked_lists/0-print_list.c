07d07b7d5c813b670a895f06cad9622929996304
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-24 18:11:47 +0000

    task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,28 +13,28 @@ size_t print_list(const list_t *h)
 	size_t count = 0;
 	const list_t *myh = h;
 
-	if (h != NULL && h -> str != NULL)
+	if (h != NULL && h->str != NULL)
 	{
-		printf("[%u] %s\n", myh -> len, myh -> str);
-		myh = myh -> next;
+		printf("[%u] %s\n", myh->len, myh->str);
+		myh = myh->next;
 		count++;
 	}
 	else if (h != NULL)
 	{
 		printf("[0] (nil)\n");
 		count++;
-		myh = myh -> next;
+		myh = myh->next;
 	}
 	while (myh != h && myh != NULL)
 	{
-		if (myh -> str == NULL)
+		if (myh->str == NULL)
 		{
 			printf("[0] (nil)\n");
 			count++;
 			continue;
 		}
-		printf("[%u] %s\n", myh -> len, myh -> str);
-		myh = myh -> next;
+		printf("[%u] %s\n", myh->len, myh->str);
+		myh = myh->next;
 		count++;
 	}
 
