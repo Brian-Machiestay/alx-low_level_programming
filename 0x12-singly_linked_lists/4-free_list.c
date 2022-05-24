9607462162d71e81ba66f2b7c53472e5d7f32b2b
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-24 21:25:04 +0000

    ending mandatory

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - check the code
+ * @head: the list
+ */
+void free_list(list_t *head)
+{
+	while (head->next != NULL && head != NULL)
+	{
+		free(head->next);
+		head = head->next;
+	}
+	free(head);
+}
