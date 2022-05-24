bb568e72001468c95ee1f6188cdfc6e8f13d9dc8
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-24 18:08:16 +0000

    task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - check the code
+ * @h: the list
+ * Return: the number of lists
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *myh = h;
+
+	if (h != NULL && h -> str != NULL)
+	{
+		printf("[%u] %s\n", myh -> len, myh -> str);
+		myh = myh -> next;
+		count++;
+	}
+	else if (h != NULL)
+	{
+		printf("[0] (nil)\n");
+		count++;
+		myh = myh -> next;
+	}
+	while (myh != h && myh != NULL)
+	{
+		if (myh -> str == NULL)
+		{
+			printf("[0] (nil)\n");
+			count++;
+			continue;
+		}
+		printf("[%u] %s\n", myh -> len, myh -> str);
+		myh = myh -> next;
+		count++;
+	}
+
+	return (count);
+}
