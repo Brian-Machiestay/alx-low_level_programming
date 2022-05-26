6fe8d2323302c26351f450b74946b85c6a227b69
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-26 08:51:37 +0000

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - check the code
+ * @head: the list
+ */
+void free_listint2(__attribute__((unused))listint_t **head)
+{
+	listint_t *myh = *head;
+	listint_t *nestmyh = NULL;
+
+	if (head != NULL)
+	{
+		myh = myh->next;
+		free(*head);
+	}
+	while (myh != *head && myh != NULL)
+	{
+		nestmyh = myh->next;
+		free(myh);
+		myh = nestmyh;
+	}
+	*head = NULL;
+}
