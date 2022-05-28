1b6294ec7e71bb70784bd17230d46dac47b9f748
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-28 09:57:50 +0000

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -55,7 +55,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *newNode;
 
 	newNode = new_Node(n);
-	if (newNode == NULL || !idx)
+	if (newNode == NULL)
 		return (NULL);
 	if (myh != NULL)
 	{
