1902e12c170150efd6fd7bb93ea4d140068d27cd
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-28 18:05:40 +0000

    task 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * reverse_listint - check the code
+ * @head: the list to reverse
+ * Return: the reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *myh = *head;
+	listint_t *ptr = *head;
+
+	while ((*head)->next != NULL)
+	{
+		myh = (*head)->next;
+		(*head)->next = myh->next;
+		myh->next = ptr;
+		ptr = myh;
+	}
+	*head = ptr;
+	return (ptr);
+}
