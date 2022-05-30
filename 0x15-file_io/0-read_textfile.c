32c4820a527055849439255afbd84da81f05d868
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-30 17:08:20 +0000

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - check the code
+ * @filename: the name of the file to read from
+ * @letters: the number of letters to read
+ * Return: the actual number of bytes read and written
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fides;
+	ssize_t refile;
+
+	buf = malloc(letters);
+	if (buf == NULL)
+		return (0);
+	if (filename == NULL)
+		return (0);
+
+	fides = open(filename, O_RDONLY);
+	if (fides < 0)
+		return (0);
+	read(fides, buf, letters);
+
+	refile = write(1, buf, letters);
+
+	if (fides < 0 || refile < 0)
+		return (0);
+	close(fides);
+	free(buf);
+	return (refile);
+}
