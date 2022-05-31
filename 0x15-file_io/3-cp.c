46d344cb56fc49ffabbde898d979db13d78cfea8
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-05-31 14:47:53 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,6 +9,56 @@
 #include <stdlib.h>
 #include "main.h"
 #define perm (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+void checkfileto(int c, char **av);
+void checkfilefrom(int c, char **av);
+void checkclose(int c, int fd);
+
+/**
+ * checkfileto - check the code
+ * @c: the # of args
+ * @av: the args
+ */
+void checkfileto(int c, char **av)
+{
+	if (c < 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+}
+
+
+/**
+ * checkfilefrom - check the code
+ * @c: the # of args
+ * @av: the args
+ */
+void checkfilefrom(int c, char **av)
+{
+	if (c < 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+}
+
+
+/**
+ * checkclose - check the code
+ * @c: the close value
+ * @fd: the file descriptor
+ */
+void checkclose(int c, int fd)
+{
+	if (c < 0)
+	{
+		dprintf(2, "Error: Can't close %d", fd);
+		exit(0);
+	}
+}
+
+
 /**
  * main - check the code
  * @ac: the # of args
@@ -19,8 +69,11 @@ int main(int ac, char **av)
 {
 	int fd;
 	int fd1;
+	int fr;
 	int fw;
 	char buf[1024];
+	int closev1;
+	int closev2;
 
 	if (ac != 3)
 	{
@@ -28,33 +81,21 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, perm);
-	if (fd1 < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
+	checkfileto(fd1, av);
 	fd = open(av[1], O_RDONLY);
-	if (fd < 0)
+	checkfilefrom(fd, av);
+	fr = read(fd, buf, 1024);
+	checkfilefrom(fr, av);
+	fw = write(fd1, buf, fr);
+	checkfileto(fw, av);
+	while (fr > 0)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		fr = read(fd, buf, 1024);
+		fw = write(fd1, buf, fr);
 	}
-	fd = read(fd, buf, 1024);
-	if (fd < 0)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", av[1]);
-		exit(98);
-	}
-	fw = write(fd1, buf, fd);
-	if (fw < 0)
-	{
-		dprintf(2, "Error: Can't write to %s\n", av[2]);
-		exit(99);
-	}
-	while (fd > 0)
-	{
-		fd = read(fd, buf, 1024);
-		fw = write(fd1, buf, fd);
-	}
+	closev1 = close(fd);
+	checkclose(closev1, fd);
+	closev2 = close(fd1);
+	checkclose(closev2, fd1);
 	return (0);
 }
