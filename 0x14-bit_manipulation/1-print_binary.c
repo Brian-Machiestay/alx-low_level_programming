5354c1dcf72c8142b2e8b10d85eb97ab88a68f3c
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-06-01 15:05:12 +0000

    task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - check the code
+ * @n: the number to convert
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 31;
+	int j = 0;
+	int count = 31;
+	int temp = 0;
+
+	if (n == 0)
+		putchar('0');
+
+	while (count > 0)
+	{
+		j = (int)((n >> (i - 1)) & 1);
+		if (j & 1)
+		{
+			temp = 1;
+			putchar('1');
+		}
+		else
+		{
+			if (temp == 1)
+				putchar('0');
+		}
+		i--;
+		count--;
+	}
+}
