2c4d699eace15150d78918467de497220d744eed
Brian-Machiestay <Brianmachiestay@gmail.com>
2022-06-01 17:31:55 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_bit - check the code
+ * @n: the integer
+ * @index: the index to get
+ * Return: a binary
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int i = 31;
+	int j = 0;
+	int count = 31;
+	unsigned int counter = 31;
+	int temp = 0;
+
+	if (n == 0 && index == 0)
+		return (0);
+
+	while (count >= 0)
+	{
+		j = (int)((n >> (i)) & 1);
+		if ((j & 1) == 1)
+		{
+			temp = 1;
+			if (index == counter)
+				return (1);
+		}
+		else
+		{
+			if (temp == 1)
+			{
+				if (index == counter)
+					return (0);
+			}
+		}
+		counter--;
+		i--;
+		count--;
+	}
+	return (-1);
+}
